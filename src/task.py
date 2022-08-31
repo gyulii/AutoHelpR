d40084858afcd03492ad8d7e1f06b24d99ffa7a2
Faragó Gyula <faragogyulii@gmail.com>
2022-08-31 14:56:40 +0200

    Function comments added

--- src/task.py
+++ src/task.py
@@ -1,3 +1,50 @@
+
+class KeyCombination:
+    """Class for managing pressed keyboard combination by updating an internal
+
+    Input: from pynput:keyboard.Key
+
+    On press event call: key_combination_pressed(key)
+
+    On release event call: key_combination_released(key)
+    """
+
+    def __init__(self, combination):
+        self.key_combination = combination
+        self.current_key_states_list = set()
+        pass
+
+    def key_combination_pressed(self, key) -> bool:
+        """Adds the currently pressed key to the class set and returns True if all key is in the list."""
+        if key in self.key_combination:
+            self.current_key_states_list.add(key)
+            if all((key in self.current_key_states_list) for key in self.key_combination):
+                return True
+            else:
+                return False
+
+    def key_combination_released(self, key):
+        if (key in self.current_key_states_list):
+            self.current_key_states_list.remove(key)
+
+
+def key_check_if_pressed(key_string, key) -> bool:
+    if 'char' in dir(key):
+        if key.char == key_string:
+            return True
+        else:
+            return False
+
+
+class Task:
+    """Have an action to execute """
+    """Have a name, functions"""
+    pass
+
+
+class SubTask(Task):
+    pass
+
 def record_mouse_position():
     """Return a 2 element list with current mouse position"""
     pass
@@ -5,35 +52,4 @@ def record_mouse_position():
 
 def read_clipboard():
     """Returns the content of the clipboard as a string"""
-    pass
-
-
-
-class KeyCombination:
-    def __init__(self , combination):
-        self.combination = combination
-        self.current_state = set()
-        pass
-    def key_combination_pressed(self, key):
-        if key in self.combination:
-            self.current_state.add(key)
-            if all(key in self.current_state for key in self.combination):
-                return True
-            else:
-                return False
-    def key_combination_released(self, key):
-        self.current_state.remove(key)
-
-
-def key_check_if_pressed(key_string , key):
-    if 'char' in dir(key):
-     if key.char == key_string:
-        return True
-    else:
-        return False
-
-
-class SubTask:
-    """Have an action to execute """
-    """Have a name, functions"""
     pass
